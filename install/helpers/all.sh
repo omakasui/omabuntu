63bbfe006c2ebdfde5236c58b5b3e53f6d2472ba
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-11 00:00:46 +0100

    Reverted the logging and error handling system to a more robust version

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source $OMAKUB_INSTALL/helpers/presentation.sh
-source $OMAKUB_INSTALL/helpers/errors.sh
 source $OMAKUB_INSTALL/helpers/logging.sh
+source $OMAKUB_INSTALL/helpers/errors.sh
--- install/helpers/presentation.sh
+++ install/helpers/presentation.sh
@@ -10,17 +10,10 @@ if ! command -v gum &> /dev/null; then
   cd -
 fi
 
-
 export LOGO_PATH="$OMAKUB_PATH/logo.txt"
 
-# Tell gum/lipgloss/termenv the terminal has a dark background (fg=15, bg=0).
-# Without this, gum sends OSC 11 queries (\033]11;?\007) to detect the
-# background color. Terminal responses leak into the input buffer and corrupt
-# subsequent gum input prompts on retry (the ^[]11;rgb:... garbage).
-export COLORFGBG="15;0"
-
 # Minimal padding for desktop terminal
-export PADDING_LEFT=2
+export PADDING_LEFT=4
 export PADDING_LEFT_SPACES=$(printf "%*s" $PADDING_LEFT "")
 
 # Tokyo Night theme for gum confirm
