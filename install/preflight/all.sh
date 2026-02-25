7b011c977f50212bbce03c4b4414c7090b06449b
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-25 17:53:04 +0100

    Add Omakasui APT repository setup scripts

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -4,4 +4,5 @@ source $OMAKUB_INSTALL/preflight/guard.sh
 source $OMAKUB_INSTALL/preflight/begin.sh
 source $OMAKUB_INSTALL/preflight/no-sleep.sh
 source $OMAKUB_INSTALL/preflight/identification.sh
+run_logged $OMAKUB_INSTALL/preflight/mirror.sh
 run_logged $OMAKUB_INSTALL/preflight/migrations.sh
---
+++ migrations/1772019826.sh
@@ -0,0 +1,11 @@
+echo "Add Omakasui APT repository"
+
+curl -fsSL https://packages.omakasui.org/omakasui.gpg.key \
+  | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/omakasui-apt.gpg > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/omakasui-apt.gpg] \
+  https://packages.omakasui.org $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") main" \
+  | sudo tee /etc/apt/sources.list.d/omakasui.list
+
+sudo apt update
