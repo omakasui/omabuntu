bc6253b61898de6f6b356a01c60dcf71b6186e5a
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-10 10:00:50 +0100

    Improve tools installation
    - Use omakasui-lazygit and omakasui-lazydocker
    - Add a mise installation script
    - Set keyrings permission

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source $OMAKUB_INSTALL/helpers/keyrings.sh
 source $OMAKUB_INSTALL/helpers/mirror.sh
 source $OMAKUB_INSTALL/helpers/presentation.sh
 source $OMAKUB_INSTALL/helpers/logging.sh
--- install/packaging/tools.sh
+++ install/packaging/tools.sh
@@ -9,11 +9,7 @@ sudo apt update
 omakub-pkg-add fastfetch
 
 # Install mise
-sudo install -dm 755 /etc/apt/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-omakub-pkg-add mise
+source ~/.local/share/omakub/applications/install/mise.sh
 
 # Install Zellij
 source ~/.local/share/omakub/applications/install/zellij.sh
@@ -21,14 +17,8 @@ source ~/.local/share/omakub/applications/install/zellij.sh
 # Install Docker
 source ~/.local/share/omakub/applications/install/docker.sh
 
-# Install LazyDocker
-source ~/.local/share/omakub/applications/install/lazydocker.sh
-
 # Install GitHub CLI
 source ~/.local/share/omakub/applications/install/github-cli.sh
-
-# Install LazyGit
-source ~/.local/share/omakub/applications/install/lazygit.sh
 
 # Install Starship
 source ~/.local/share/omakub/applications/install/starship.sh
