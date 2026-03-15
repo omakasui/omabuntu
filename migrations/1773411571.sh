97cd2d9befe469331d022e62684a1d9a11cbe50c
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-15 01:03:15 +0100

    Add background menu option and update keybindings for Walker migration

--- migrations/1773411571.sh
+++ migrations/1773411571.sh
@@ -1,3 +1,8 @@
 echo "Migrate to Walker from Wofi"
 omakub-pkg-add omakasui-walker
 omakub-pkg-drop wofi
+
+# Set omakub next background to Super+Shift+Control
+omakub-keybinding-remove 'Omakub Background Next'
+omakub-keybinding-remove 'Omabuntu Background Next'
+omakub-keybinding-add 'Omabuntu Background Next' 'omakub-menu background' '<Super><Control>space'
