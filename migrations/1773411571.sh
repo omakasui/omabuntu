c86ed76e73b63238db06302f869a5f6948dc9d51
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-16 13:33:26 +0100

    Enhance migration script to apply Walker and Elephant configurations immediately

--- migrations/1773411571.sh
+++ migrations/1773411571.sh
@@ -2,10 +2,23 @@ echo "Migrate to Walker from Wofi"
 omakub-pkg-add omakasui-walker
 omakub-pkg-drop wofi
 
+# Apply Walker and Elephant changes immediately
+mkdir -p ~/.config/walker/
+cp -f $OMAKUB_PATH/config/walker/config.toml ~/.config/walker/config.toml
+
+mkdir -p ~/.config/elephant/
+cp -f $OMAKUB_PATH/config/elephant/calc.toml ~/.config/elephant/calc.toml
+cp -f $OMAKUB_PATH/config/elephant/desktopapplications.toml ~/.config/elephant/desktopapplications.toml
+cp -f $OMAKUB_PATH/config/elephant/symbols.toml ~/.config/elephant/symbols.toml
+
+bash $OMAKUB_PATH/install/config/walker-elephant.sh
+
 # Set omakub next background to Super+Shift+Control
 omakub-keybinding-remove 'Omakub Background Next'
 omakub-keybinding-remove 'Omabuntu Background Next'
 omakub-keybinding-add 'Omabuntu Background Next' 'omakub-menu background' '<Super><Control>space'
+
+bash $OMAKUB_PATH/install/first-run/elephant.sh
 
 # Require a logout to apply the new keybinding and menu changes
 omakub-state set logout-required
