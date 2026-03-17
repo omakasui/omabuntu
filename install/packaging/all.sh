42422c017ae13a08e9b98b13c096d51cd017f869
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-17 12:55:36 +0100

    Reorder npx script execution in installation process

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -6,9 +6,8 @@ run_logged $OMAKUB_INSTALL/packaging/pipx.sh
 run_logged $OMAKUB_INSTALL/packaging/fonts.sh
 run_logged $OMAKUB_INSTALL/packaging/nvim.sh
 run_logged $OMAKUB_INSTALL/packaging/tools.sh
-run_logged $OMAKUB_INSTALL/packaging/npx.sh
 run_logged $OMAKUB_INSTALL/packaging/icons.sh
 run_logged $OMAKUB_INSTALL/packaging/apps.sh
 run_logged $OMAKUB_INSTALL/packaging/webapps.sh
 run_logged $OMAKUB_INSTALL/packaging/tuis.sh
-
+run_logged $OMAKUB_INSTALL/packaging/npx.sh
