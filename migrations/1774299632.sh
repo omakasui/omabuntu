7772434957f768f83771ac8d0c02e4f960985407
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-23 22:23:23 +0100

    Replace omakub-theme-refresh with omakub-restart-tmux in Tmux migration script

--- migrations/1774299632.sh
+++ migrations/1774299632.sh
@@ -5,7 +5,7 @@ omakub-pkg-add tmux
 if [[ ! -f ~/.config/tmux/tmux.conf ]]; then
   mkdir -p ~/.config/tmux
   cp $OMAKUB_PATH/config/tmux/tmux.conf ~/.config/tmux/tmux.conf
-  omakub-theme-refresh
+  omakub-restart-tmux
 fi
 
 echo "Add Tmux binding (Super+Alt+Return)"
