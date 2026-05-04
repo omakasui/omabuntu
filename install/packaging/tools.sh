4f14ff10fffc25fb5e2c4c5bdf9c4bd76a69f9fa
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-05-04 23:56:37 +0200

    Upgrade docker to core package

--- install/packaging/tools.sh
+++ install/packaging/tools.sh
@@ -3,8 +3,5 @@
 # Install mise
 source ~/.local/share/omakub/applications/install/mise.sh
 
-# Install Docker
-source ~/.local/share/omakub/applications/install/docker.sh
-
 # Install GitHub CLI
 source ~/.local/share/omakub/applications/install/github-cli.sh
