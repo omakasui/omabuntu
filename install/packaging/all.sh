7278130455ea445c04ad51d339b8129b934326ed
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-05-04 23:33:57 +0200

    Move browser installation scripts

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -8,7 +8,7 @@ run_logged $OMAKUB_INSTALL/packaging/fonts.sh
 run_logged $OMAKUB_INSTALL/packaging/nvim.sh
 run_logged $OMAKUB_INSTALL/packaging/tools.sh
 run_logged $OMAKUB_INSTALL/packaging/icons.sh
-run_logged $OMAKUB_INSTALL/packaging/apps.sh
+run_logged $OMAKUB_INSTALL/packaging/chromium.sh
 run_logged $OMAKUB_INSTALL/packaging/webapps.sh
 run_logged $OMAKUB_INSTALL/packaging/tuis.sh
 run_logged $OMAKUB_INSTALL/packaging/npx.sh
