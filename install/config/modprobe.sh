f3c81ad21d571fe32f90bb95bc350eb0bc817236
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-05-08 15:06:55 +0200

    Add script to disable ESP4, ESP6, and RXRPC modules to mitigate Dirty Frag exploit

--- install/config/modprobe.sh
+++ install/config/modprobe.sh
@@ -6,3 +6,14 @@ if [ ! -f $DISABLE_ALGIF ]; then
   sudo rmmod algif_aead 2>/dev/null || true
   sudo update-initramfs -u
 fi
+
+DISABLE_DIRTYFRAG=/etc/modprobe.d/disable-esp4-esp6-rxrpc.conf
+if [ ! -f $DISABLE_DIRTYFRAG ]; then
+  /usr/share/ohmydebn/bin/ohmydebn-headline "cat" "Disabling multiple kernel modules to mitigate Dirty Frag exploit"
+  echo "install esp4 /bin/false" | sudo tee $DISABLE_DIRTYFRAG
+  echo "install esp6 /bin/false" | sudo tee -a $DISABLE_DIRTYFRAG
+  echo "install rxrpc /bin/false" | sudo tee -a $DISABLE_DIRTYFRAG
+  echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+  sudo rmmod esp4 esp6 rxrpc 2>/dev/null || true
+  sudo update-initramfs -u
+fi
---
+++ migrations/1778132453.sh
@@ -0,0 +1,3 @@
+echo "Disabling ESP4, ESP6, and RXRPC kernel modules to mitigate Dirty Frag exploit"
+
+source "$OMAKUB_PATH/install/config/modprobe.sh"
