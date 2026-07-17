92cef9a0ee2c77d792167806d234e281e3c7c127
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-07-17 18:39:53 +0200

    fix: missing branding copy

--- install/config/branding.sh
+++ install/config/branding.sh
@@ -1,4 +1,5 @@
 # Allow the user to change the branding
 mkdir -p ~/.config/omakub/branding
+cp ~/.local/share/omakub/brand ~/.config/omakub/branding/brand
 cp ~/.local/share/omakub/icon.png ~/.config/omakub/branding/notify.png
 cp ~/.local/share/omakub/icon.png ~/.config/omakub/branding/icon-launcher.png
